d4585888053c3841875d6654480d76a2d3d0943b
Emily Wu <chinadragon.emily@gmail.com>
2024-01-26 23:04:41 -0500

    Fix documentation on Timer

--- src/main/java/sketch_practice/model/SketchCountdownTimer.java
+++ src/main/java/sketch_practice/model/SketchCountdownTimer.java
@@ -3,7 +3,7 @@ package sketch_practice.model;
 import sketch_practice.util.Observable;
 
 /*
-This timer is used to keep
+This timer is used as a simple countdown timer.
  */
 public class SketchCountdownTimer extends Observable {
 }
