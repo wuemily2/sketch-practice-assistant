eda8c54f443983a43a740eaccf870936928f05ae
Emily Wu <chinadragon.emily@gmail.com>
2024-01-27 01:01:21 -0500

    Change deprecated Observer pattern code to personal implementation

--- src/main/java/sketch_practice/util/Observable.java
+++ src/main/java/sketch_practice/util/Observable.java
@@ -6,11 +6,17 @@ import java.util.ArrayList;
 /*
 TODO: replace with earlier implementation because Observable is deprecated
  */
-public class Observable extends java.util.Observable {
-
-    @Override
+public class Observable {
+    private ArrayList<Observer> observers = new ArrayList<Observer>();
+    public void attach(Observer o) {
+        observers.add(o);
+    }
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
     public void notifyObservers() {
-        this.setChanged();
-        super.notifyObservers();
+        for(Observer o:observers) {
+            o.update(this);
+        }
     }
 }
