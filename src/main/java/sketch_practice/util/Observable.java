602ba051b44c0117e09ecf13ddf85f5b4a1f5123
Emily Wu <chinadragon.emily@gmail.com>
2024-01-27 00:58:41 -0500

    Add TODO note for Observable class using deprecated Java observable utility
    We must remove references to the deprecated classes and implement our own observable utility

--- src/main/java/sketch_practice/util/Observable.java
+++ src/main/java/sketch_practice/util/Observable.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 
 /*
-We'll use this in conjunction with java.util.Observer
+TODO: replace with earlier implementation because Observable is deprecated
  */
 public class Observable extends java.util.Observable {
 
