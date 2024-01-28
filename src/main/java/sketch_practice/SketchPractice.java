9e83b1be47f31f3a7543b05a549241cfd07da7bf
Emily Wu <chinadragon.emily@gmail.com>
2024-01-28 12:59:29 -0500

    Rewrite main application file to wire the model view and controller together and put GUI placeholder code for scene switching that is functional

--- src/main/java/sketch_practice/SketchPractice.java
+++ src/main/java/sketch_practice/SketchPractice.java
@@ -1,6 +1,8 @@
 package sketch_practice;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -9,12 +11,77 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.DirectoryChooser;
-import java.io.File;
-import java.util.Arrays;
+import javafx.stage.WindowEvent;
+import sketch_practice.controller.CyclerController;
+import sketch_practice.model.ImageCycler;
+import sketch_practice.model.ImageFileFinder;
+import sketch_practice.model.JavaFXCountdownTimer;
+import sketch_practice.model.CountdownTimer;
+import sketch_practice.view.SketchPracticeGUI;
 
-public class SketchPractice {
-    public static void main(String [] args){
+public class SketchPractice extends Application {
+    public static void main(String [] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage stage) {
+        // Give the Application a name in the top bar
+        stage.setTitle("Sketch Practice Assistant");
+        //Ensure stage closes properly
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                Platform.exit();//Terminate JavaFX Application Thread
+                System.exit(0); // Close Java process
+            }
+        });
+        //Create model components
+        ImageFileFinder fileFinder = new ImageFileFinder();
+        CountdownTimer timer = new JavaFXCountdownTimer();
+        // Setup controller
+        CyclerController controller = new CyclerController(fileFinder, timer);
+        //Setup stage in View object, with controller to setup event handlers
+        SketchPracticeGUI view = new SketchPracticeGUI(stage, controller);
+        //Attach observers to observables
+        fileFinder.attach(view);
+        timer.attach(view);
+
+        //Finally, show the current scene.
+        view.showStage();
+        /*CountdownTimer timer = new JavaFXCountdownTimer(600);
+        timer.attach(view);
+
+        stage.setTitle("New Scene");
+        //Create view in Java
+        Label title = view.timeLabel;
+        TextField textField = new TextField("Enter your name here");
+        Button button = new Button("OK");
+
+        VBox container = new VBox(title, textField, button);
+        //Style container
+        container.setSpacing(15);
+        container.setPadding(new Insets(25));
+        container.setAlignment(Pos.CENTER);
+        *//*button.setOnAction(event -> {
+            DirectoryChooser directoryChooser = new DirectoryChooser(); //May need to use Jfilechooser
+            File selectedDirectory = directoryChooser.showDialog(this.stage);
+            if (selectedDirectory != null) {
+                title.setText(selectedDirectory.getName());
+                File[] FilesList = selectedDirectory.listFiles();
+                if (FilesList == null) {
+                    return;
+                }
+                for (File file : FilesList) {
+                    System.out.println(file.toString());
+                }
+            }
+        });*//*
+        //Set view in window
+        stage.setScene(new Scene(container));
+        //Launch
+        stage.show();
+        timer.startTimer();
+        System.out.println(timer.getTimeLeftAsString());*/
     }
 }
--- src/main/java/sketch_practice/view/SketchPracticeGUI.java
+++ src/main/java/sketch_practice/view/SketchPracticeGUI.java
@@ -1,68 +1,107 @@
 package sketch_practice.view;
 
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.stage.DirectoryChooser;
+import sketch_practice.controller.CyclerController;
+import sketch_practice.model.CountdownTimer;
 import sketch_practice.util.Observable;
 import sketch_practice.util.Observer;
 import javafx.stage.Stage;
 
-import java.io.File;
 
-public class SketchPracticeGUI extends Application implements Observer {
+
+public class SketchPracticeGUI implements Observer {
     private Stage stage;
-    public void switchView(){
+    private Scene masterScene;
 
+    private SettingsGUI settingsGUI;
+    private CyclingGUI cyclingGUI;
+
+    private CyclerController controller;
+
+    //
+    public Label timeLabel = new Label("TimeLabel");
+    public Label userMessageLabel = new Label("Hello!");
+
+    class SettingsGUI extends Pane {
+
+        private EventHandler<ActionEvent> removal_handler;
+        private Button removalButton = new Button("Remove directory.");
+        public SettingsGUI (){ // build self
+
+            // TODO Remove test
+            Button switchbutton = new Button("SettingsGui");
+            switchbutton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    SketchPracticeGUI.this.switchView();
+                }
+            });
+            this.getChildren().add(switchbutton);
+            //
+
+            // Make children first and then put into node list of VBOX
+
+            //Create VBOX
+            //VBox settingsList = new VBox();
+
+            //this.getChildren().add(settingsList);
+        }
+
+
+    }
+
+    class CyclingGUI extends Pane{
+        public CyclingGUI (){
+            Button switchbutton = new Button("CyclingGui");
+            switchbutton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    SketchPracticeGUI.this.switchView();
+                }
+            });
+            this.getChildren().add(switchbutton);
+        }
+    }
+
+    // Take in a stage created by the application on the JavaFX thread; Make sure to instantiate on same thread
+    public SketchPracticeGUI(Stage applicationStage, CyclerController controller){
+        this.controller = controller;
+        this.stage = applicationStage;
+        //TODO - implement the following guis
+        this.settingsGUI = new SettingsGUI();
+        this.cyclingGUI = new CyclingGUI();
+        //
+        this.masterScene = new Scene(settingsGUI); // Initially start the app with a settings GUI
+        this.stage.setScene(this.masterScene);
+    }
+
+    public void switchView(){ // Switch between the scenes.
+        this.masterScene.setRoot(
+                this.masterScene.getRoot() == settingsGUI ? cyclingGUI : settingsGUI);
     }
     public Stage getStage(){
         return this.stage;
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public void showStage(){
+        stage.show();
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        //Create Stage
-        Stage newWindow = stage;
-        newWindow.setTitle("New Scene");
-        //Create view in Java
-        Label title = new Label("This is a pretty simple view!");
-        TextField textField = new TextField("Enter your name here");
-        Button button = new Button("OK");
-        button.setOnAction(event -> {
-            DirectoryChooser directoryChooser = new DirectoryChooser(); //May need to use Jfilechooser
-            File selectedDirectory = directoryChooser.showDialog(newWindow);
-            if (selectedDirectory != null) {
-                title.setText(selectedDirectory.getName());
-                File[] FilesList = selectedDirectory.listFiles();
-                if (FilesList == null) {
-                    return;
-                }
-                for (File file : FilesList) {
-                    System.out.println(file.toString());
-                }
-            }
-        });
-        VBox container = new VBox(title, textField, button);
-        //Style container
-        container.setSpacing(15);
-        container.setPadding(new Insets(25));
-        container.setAlignment(Pos.CENTER);
-        //Set view in window
-        newWindow.setScene(new Scene(container));
-        //Launch
-        newWindow.show();
+    public void hideStage(){
+        stage.hide();
     }
 
     public void update(Observable o){
+        if(o instanceof CountdownTimer){
+            CountdownTimer timer = (CountdownTimer) o;
+            timeLabel.setText(timer.getTimeLeftAsString());
+        }
 
     }
 }
